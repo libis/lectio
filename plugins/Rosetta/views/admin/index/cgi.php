1d8a809cb63b9ca097424d8c558961b394402b0a
joris <joris.lambrechts@bib.kuleuven.be>
2015-09-28 15:08:14 +0200

    rosetta plugin searches using list and pid instead op query and title

--- plugins/Rosetta/views/admin/index/cgi.php
+++ plugins/Rosetta/views/admin/index/cgi.php
@@ -12,39 +12,27 @@
     endif;    
 
     $http_client->setUri(
-        $base_url."/search?query=IE.dc.title=".urlencode($_GET['search'])."&step=100"
+        $base_url."/".urlencode($_GET['search'])."/list"
     );
 
     $http_response = $http_client->request();
-    $data = $http_response->getBody();    
+    $list = $http_response->getBody();    
     
-    if($data):
-        $images = json_decode($data);    
-           var_dump($data);
-        //create form
-        $size = sizeof($images);
-
-        $html = "<div class='result'>";
-        foreach ($images->set as $image):      
-            $html .="<div class='rosetta_image'><img src='".$base_url.$image->pid."'/><Input type = 'Radio' Name ='pid' value= '".$image->pid."'>
-            </div>"; 
-
-            //get list
-            $http_client->setUri($base_url."/".$image->pid."/list");
-            $http_response = $http_client->request();
-            $list = $http_response->getBody();
-            if($list):
-                $list_images = json_decode($list); 
-                if(sizeof($list_images) > 1):
-                    foreach ($list_images as $pid):
-                        $pid = (array)$pid;
-                        $pid = key($pid);
-                        $html .="<div class='rosetta_image child'><img src='".$base_url.$pid."'/><Input type = 'Radio' Name ='pid' value= '".$pid."'>
-                        </div>"; 
-                    endforeach;
-                endif;
-            endif;
-        endforeach;
+    if($list):
+        $html = "<div class='result'>";        
+        
+        $list_images = json_decode($list);    
+        $array = (array)$list_images;
+        $list_images= (array)$array[$_GET['search']];
+        var_dump($list_images);
+        if(sizeof($list_images) > 1):
+            foreach ($list_images as $key => $value):
+                
+                $html .="<div class='rosetta_image child'><img alt='".$value."' src='".$base_url."/".$key."'/><Input type = 'Radio' Name ='pid' value= '".$key."'>
+                </div>"; 
+            endforeach;
+        endif;
+               
         $html .= "</div>";
 
         echo $html;
