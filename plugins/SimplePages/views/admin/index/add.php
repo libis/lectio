9ac6da651819bbfb45e293d04196bc4df09e4abc
joris <joris.lambrechts@bib.kuleuven.be>
2015-10-08 10:07:30 +0200

    fix #15

--- plugins/SimplePages/views/admin/index/add.php
+++ plugins/SimplePages/views/admin/index/add.php
@@ -19,7 +19,7 @@ jQuery(window).load(function() {
         theme: 'advanced',
         theme_advanced_toolbar_location: 'top',
         theme_advanced_toolbar_align: 'left',
-        theme_advanced_buttons3_add : 'pastetext,pasteword,selectall',
+        theme_advanced_buttons3_add : 'pastetext,pasteword,selectall,|,forecolor,backcolor',
         // Allow object embed. Used by media plugin
         // See http://www.tinymce.com/forum/viewtopic.php?id=24539
         media_strict: false,
