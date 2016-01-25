12cd4fc036083eeb781b1b3ec78dd3fa707ca6d0
joris <joris.lambrechts@bib.kuleuven.be>
2016-01-25 13:41:29 +0100

    #11 voorkomen dat site opent in compatibility mode van IE

--- themes/lectio/common/header.php
+++ themes/lectio/common/header.php
@@ -3,6 +3,7 @@
 <head>
     <meta charset="utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <?php if ($description = option('description')): ?>
     <meta name="description" content="<?php echo $description; ?>">
     <?php endif; ?>
